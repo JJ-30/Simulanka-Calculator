a2bf43a3de9514dbb0ff3f0f84bd673ceaea2445
JJ-30 <163680409+JJ-30@users.noreply.github.com>
2024-11-02 15:38:26 -0400

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,71 +1,98 @@
-var userInput = []
-var arrCalculate = []
+// Variables that wait for the user to insert and confirm numbers, beginning as empty string values and being converted into integer values for calculation.
+var numberA = ''
+var numberB = ''
 
+// Var n used to track what numbers the user selects, and var numberA and numberB.
+var n = ''
+const b = false
+
+// Counter variable to
 numbers = 0
 
-let testing = () => {
-
-    let obj1 = document.getElementById("test");
-    obj1.innerHTML = parseInt(userInput.join(''), 10);
-
-}
 
 
-// This function pushes the first number the user wants to calculate into arrCalculate, and allows the user to insert another number.
-let pushElement = () => {
-
+// These functions pushes the number the user wants to calculate into, in order of pushElementA -> pushElementB. This uses button disabling and enabling to prevent users
+// from reaccessing numbers that were already inserted.
+let pushElementA = () => {
     if (numbers > 1) {
         let obj1 = document.getElementById("test");
         obj1.innerHTML = "You have exceeded the maximum numbers, please reset or choose a calculation method."} else 
-        { arrCalculate.push(parseInt(userInput.join(''), 10));
-            userInput = [] }
-    displayNumbers()
-
+        { 
+            numberA += n;
+            numberA == Number(numberA);
+            n = '';
+            b == true;
+            var btn = document.getElementById("buttonB");
+            btn.disabled = false;
+            var btn = document.getElementById("buttonA");
+            btn.disabled = true;
+        }
+}
+let pushElementB = () => {
+    if (numbers > 1) {
+        let obj1 = document.getElementById("test");
+        obj1.innerHTML = "You have exceeded the maximum numbers, please reset or choose a calculation method."} else 
+        { 
+            numberB += n;
+            numberB == Number(numberB);
+            n == '';
+            var btn = document.getElementById("buttonB");
+            btn.disabled = true;
+        }
 }
 
+
+
+// Listens to whenever a user clicks a number to display it on the textfield, checking the button status to display in number A or number B.
 let displayNumbers = () => {
-    let numberA = document.getElementById("numberA")
-    let numberB = document.getElementById("numberB")
-    if (numberA.innerHTML = "Your first number") {
-        numberA.innerHTML = arrCalculate[0]
-    }
-    if (numberA.innerHTML != "Your first number" && arrCalculate.length > 1) {
-        numberB.innerHTML = arrCalculate[1]
+    let numberX = document.getElementById("numberA")
+    let numberY = document.getElementById("numberB")
+    var btn = document.getElementById("buttonB");
+    if (btn.disabled == true) {
+        numberX.innerHTML = n;
+    } else if (btn.disabled == false) {
+        numberY.innerHTML = n;
     } else {numberB.innerHTML = "Your second number"}
 }
 
+// Basic calculation functions start
 let multiply = () => {
-    let m = arrCalculate[0] * arrCalculate[1];
+    let m = numberA * numberB;
     let obj1 = document.getElementById("test");
     obj1.innerHTML = m
 }
 
 let divide = () => {
-    let m = arrCalculate[0] / arrCalculate[1];
+    let m = numberA / numberB;
     let obj1 = document.getElementById("test");
     obj1.innerHTML = m
 }
 
 let subtract = () => {
-    let m = arrCalculate[0] - arrCalculate[1];
+    let m = numberA - numberB;
     let obj1 = document.getElementById("test");
     obj1.innerHTML = m
 }
 
 let add = () => {
-    let m = arrCalculate[0] + arrCalculate[1];
+    let m = Number(numberA) + Number(numberB);
     let obj1 = document.getElementById("test");
     obj1.innerHTML = m
 }
+// Basic calculation functions end
 
+// Resets entire calculator history to redo calculations
 let reset = () => {
-    userInput = []
-    arrCalculate = []
+    numberA = ''
+    numberB = ''
+    n = ''
     let obj1 = document.getElementById("test");
-    let numberA = document.getElementById("numberA")
-    let numberB = document.getElementById("numberB")
+    let numberX = document.getElementById("numberA")
+    let numberY = document.getElementById("numberB")
     obj1.innerHTML = "Result"
-    numberA.innerHTML = "Your first number";
-    numberB.innerHTML = "Your second number";
+    numberX.innerHTML = "Your first number";
+    numberY.innerHTML = "Your second number";
     numbers = 0
+    var btn = document.getElementById("buttonB");
+    btn.disabled = true;
 }
